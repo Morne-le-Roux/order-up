ae4a1a5f2d37a42380881da82462d406101c80c1
Morné le Roux <mornelerx@gmail.com>
2023-08-15 19:39:43 +0200

    updated secondary color

--- lib/constants.dart
+++ lib/constants.dart
@@ -2,6 +2,6 @@ import 'package:flutter/material.dart';
 
 const kBackgroundColor = Colors.pinkAccent;
 
-const kSecondaryColor = Color.fromARGB(255, 251, 219, 230);
+const kSecondaryColor = Color.fromARGB(255, 252, 243, 246);
 
 const kPrimaryColor = Colors.pink;
