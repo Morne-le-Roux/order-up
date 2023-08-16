5aca356f959a7d1a014c3de3a8e3b4adb3c6b7ed
Morné le Roux <mornelerx@gmail.com>
2023-08-16 07:32:32 +0200

    added counter prototype

--- lib/components/menu_item.dart
+++ lib/components/menu_item.dart
@@ -18,27 +18,52 @@ class MenuItem extends StatefulWidget {
 class _MenuItemState extends State<MenuItem> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(20),
-          border: Border.all(color: kPrimaryColor, width: 2),
-          color: kSecondaryColor,
-        ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Container(
-                padding: const EdgeInsets.only(left: 25, right: 25, top: 10),
-                child: FittedBox(child: widget.icon)),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Text(
-                widget.name,
-                textAlign: TextAlign.center,
-                style: const TextStyle(color: kPrimaryColor),
-              ),
+    return Stack(
+      alignment: Alignment.topRight,
+      children: [
+        Container(
+            margin: const EdgeInsets.all(10),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20),
+              border: Border.all(color: kPrimaryColor, width: 2),
+              color: kSecondaryColor,
             ),
-          ],
-        ));
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Container(
+                    padding:
+                        const EdgeInsets.only(left: 25, right: 25, top: 10),
+                    child: FittedBox(child: widget.icon)),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                    widget.name,
+                    textAlign: TextAlign.center,
+                    style: const TextStyle(color: kPrimaryColor),
+                  ),
+                ),
+              ],
+            )),
+        const AmountCounter(),
+      ],
+    );
+  }
+}
+
+class AmountCounter extends StatelessWidget {
+  const AmountCounter({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 40,
+      width: 40,
+      decoration: BoxDecoration(
+          color: kSecondaryColor,
+          border: Border.all(color: Colors.black),
+          borderRadius: BorderRadius.circular(50)),
+      child: const Center(child: Text("42")),
+    );
   }
 }
