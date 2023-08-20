517aaa70eefb6f839938f29cb26bb94f9f6a494c
Morné le Roux <mornelerx@gmail.com>
2023-08-20 11:50:01 +0200

    changed colors in textboxes

--- lib/constants.dart
+++ lib/constants.dart
@@ -2,12 +2,12 @@ import 'package:flutter/material.dart';
 
 const kBackgroundColor = Colors.pinkAccent;
 
-const kSecondaryColor = Color.fromARGB(255, 252, 243, 246);
+const kSecondaryColor = Color.fromARGB(71, 252, 243, 246);
 
 const kPrimaryColor = Colors.pink;
 
 const kHintTextStyle = TextStyle(
-  color: Color.fromARGB(138, 246, 172, 197),
+  color: Color.fromARGB(211, 246, 172, 197),
   fontSize: 16,
 );
 
