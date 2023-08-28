3e558e66389db3ce0e8012f8828b65ca5a660c5a
Morné le Roux <mornelerx@gmail.com>
2023-08-28 18:43:50 +0200

    init constants

---
+++ lib/constants.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+const kBackgroundColor = Colors.pinkAccent;
+
+const kSecondaryColor = Color.fromARGB(71, 252, 243, 246);
+
+const kPrimaryColor = Colors.pink;
+
+const kHintTextStyle = TextStyle(
+  color: Color.fromARGB(211, 246, 172, 197),
+  fontSize: 16,
+);
+
+const kPrimaryTextStyle = TextStyle(
+  fontWeight: FontWeight.bold,
+  fontSize: 20,
+);
